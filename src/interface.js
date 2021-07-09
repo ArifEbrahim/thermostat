ce680feb536e4e22ae0f5ec951288521e914513b
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-07-09 13:12:52 +0100

    refactored to arrow methods

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  const updateTemperature = function() {
+  const updateTemperature = () => {
     document.querySelector('#temperature').innerText = thermostat.temperature
     document.querySelector('#temperature').className = thermostat.energyUsage();
   };
@@ -8,29 +8,29 @@ document.addEventListener("DOMContentLoaded", function() {
   const thermostat = new Thermostat();
   updateTemperature();
 
-  document.querySelector('#temperature-up').addEventListener('click', function(){
+  document.querySelector('#temperature-up').addEventListener('click', () => {
     thermostat.up();
     updateTemperature();
   });
 
-  document.querySelector('#temperature-down').addEventListener('click', function() {
+  document.querySelector('#temperature-down').addEventListener('click', () => {
     thermostat.down();
     updateTemperature();
   });
 
-  document.querySelector('#temperature-reset').addEventListener('click', function() {
+  document.querySelector('#temperature-reset').addEventListener('click', () => {
     thermostat.resetTemperature();
     updateTemperature();
   });
 
-  document.querySelector('#powersaving-on').addEventListener('click', function() {
+  document.querySelector('#powersaving-on').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOn();
     document.querySelector('#powersaving-on').style.borderColor = 'black';
     document.querySelector('#powersaving-off').style.borderColor = 'white';
     alert('Maximum temperature limited to 25 degrees');
   });
 
-  document.querySelector('#powersaving-off').addEventListener('click', function() {
+  document.querySelector('#powersaving-off').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOff();
     document.querySelector('#powersaving-on').style.borderColor = 'white';
     document.querySelector('#powersaving-off').style.borderColor = 'black';
@@ -41,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function() {
     event.preventDefault();
     const city = document.querySelector('#current-city').value;
     displayWeather(city);
-
   });
 
   const displayWeather = (city) => {
@@ -49,11 +48,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     fetch(url)
     .then(response => response.json())
-    .then(data => document.querySelector('#current-temperature').innerText = Math.round(data.main.temp))
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => document.querySelector('#current-location').innerText = data.name)
+    .then(data => {
+      document.querySelector('#current-temperature').innerText = Math.round(data.main.temp)
+      document.querySelector('#current-location').innerText = data.name
+    });
 
   };
   displayWeather('London');
